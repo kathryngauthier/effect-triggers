1b8ede549ef2ac082a67c8ce4c5acbab4b62fddc
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-04-07 21:01:27 -0400

    finished

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var $body = $('body');
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
+
+$btnShowHide.on('click', function () {
+  $box.toggleClass('showbox');
+});
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('movein');
+});
+
+$btnCollapse.on('click', function () {
+  $panel.toggleClass('collapse');
+});
+
+$btnBounce.on('click', function () {
+  $circle.addClass('bounce');
+});
+
+$btnBounce.on('animationend', function () {
+  $circle.removeClass('bounce');
+});
+
+$btnAppend.on('click', function () {
+  $list.append('<li>New List Item</li>');
+});
